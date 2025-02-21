35e5777a84d194e4251adfebdde693904f065795
Ibrahim <ibrahimmiishaq@gmail.com>
2025-02-20 22:06:58 -0800

    Log Section Complete

---
+++ Assets/Scripts/MovingLogs.cs
@@ -0,0 +1,23 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class MovingLogs : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
